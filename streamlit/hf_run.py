1e63735f419851ce9408b92c951a217fddf895d5
June <sanjunah16@snu.ac.kr>
2024-10-08 05:56:55 +0000

    .

--- streamlit/hf_run.py
+++ streamlit/hf_run.py
@@ -1,24 +1,36 @@
 import streamlit as st
+import requests
 
-def run_model(model_info, model_input):
-    # 모델 실행 로직 구현
-    model_output = model_info + model_input
-    return model_output
+# HuggingFace API call function
+def run_model(model_id, access_token, prompt):
+    API_URL = f"https://api-inference.huggingface.co/models/{model_id}"
+    headers = {"Authorization": f"Bearer {access_token}"}
+    payload = {
+        "inputs": prompt,
+    }
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
 
-# 제목 설정
-st.title("HuggingFace model Run")
+# Title
+st.title("HuggingFace Serverless API Inference")
 
-# 모델 정보 입력
-model_info = st.text_input("model id")
+# hyperlink for further information
+st.markdown("### [Available HuggingFace Models](https://huggingface.co/models?inference=warm&pipeline_tag=text-generation&sort=trending)")  
+st.markdown("[For more details, please refer to the HuggingFace API documentation](https://huggingface.co/docs/api-inference/parameters)")  
 
-# 모델 input
+# model_id, access_token input
+model_id = st.text_input("Model ID")
+access_token = st.text_input("Access Token", type="password")
+
+# model input
 model_input = st.text_input("Input")
 
-# 모델 실행 
-model_output = run_model(model_info, model_input)
-
-# 모델 실행 결과 출력
-st.write(f"Output: {model_output}")
+# Generate
+if st.button("Generate"):
+    model_output = run_model(model_id, access_token, model_input)
+    st.write("Output:")
+    st.write(model_output)
 
 # install streamlit : conda install -c conda-forge streamlit
+# install requests : conda install -c conda-forge requests
 # run this command in terminal : streamlit run hf_run.py
